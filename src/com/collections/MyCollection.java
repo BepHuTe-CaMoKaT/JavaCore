5b193870ba90cb16e22edf31ba94545ef9612e2e
Nick <eban7346@gmail.com>
2020-07-26 19:46:47 +0300

    qq
    qq

--- src/com/collections/MyCollection.java
+++ src/com/collections/MyCollection.java
@@ -12,59 +12,76 @@
 package com.collections;
 
 import java.util.ArrayList;
+import java.util.List;
 
 class MyCollection extends ArrayList<Integer> {
+    private static List<Integer> values = new ArrayList<>();
+
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(5);
-        System.out.println(arrayList);
-    }
-    public int getIndex(int index) {
-        return get(index);
+        values.add(1);
+        values.add(2);
+        values.add(3);
+        values.add(5);
+        values.remove(1);
+        System.out.println(values);
+
     }
 
-    @Override
+    public MyCollection(List<Integer> values) {
+        MyCollection.values = values;
+    }
+
+    public int getIndex(int index) {
+        return values.get(index);
+    }
+
+    
     public boolean add(Integer value) {
-        for (int i = 0; i < size(); i++) {
-            set(i,get(i)+value);
+        for (Integer i : values) {
+            i += value;
         }
-        return super.add(value);
+        return values.add(value);
     }
 
     public boolean remove(Integer value) {
-        for (int j = 0; j < size(); j++) {
-            set(j, get(j) - value);
-        }
-        return super.remove(value);
-    }
 
-    public double getAverage(){
+        for (Integer i : values) {
+            i -= value;
+        }
+        return values.remove(value);
+    }
+//    @Override
+//    public boolean add(Integer integer) {
+//        return super.add(integer);
+//    }
+
+
+    public double getAverage() {
         int sum = 0;
         double avg;
-        for(int i = 0; i < size(); i++)
+        for (int i = 0; i < values.size(); i++)
             sum += getIndex(i);
-        avg = (float)sum / size();
+        avg = (float) sum / values.size();
         return avg;
     }
 
-    public int maxValue(){
+    public int maxValue() {
         int max = 0;
-        for(int i = 0; i < size(); i++) {
+        for (int i = 0; i < values.size(); i++) {
             if (getIndex(i) > max)
                 max = getIndex(i);
         }
         return max;
     }
 
-    public int minValue(){
+    public int minValue() {
         int min = getIndex(0);
-        for(int i = 0; i < size(); i++) {
+        for (int i = 0; i < values.size(); i++) {
             if (getIndex(i) < min)
                 min = getIndex(i);
         }
         return min;
     }
+
+
 }
