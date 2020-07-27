a47fcb01dc472ebf14243de50f6ea9eaf0e3aff9
Nick <eban7346@gmail.com>
2020-07-27 15:54:10 +0300

    q1
    find sum

--- src/com/collections/ArraySumIdentification.java
+++ src/com/collections/ArraySumIdentification.java
@@ -19,10 +19,3 @@ public class ArraySumIdentification {
         return null;
     }
 }
-
-//for (int i = 0; i < arr.length; i++) {
-//        for (int j = i+1; j < arr.length; j++) {
-//        if (i+j==number) return new int[]{arr[i],arr[j]};
-//        }
-//        }
-//        return null;
